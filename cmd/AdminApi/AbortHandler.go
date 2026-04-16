89242ad06ad8d88d1d1734dd5450669ce76a70ae
moelkasaby004 <mohamed.alkspynm@gmail.com>
2026-04-16 20:54:51 +0200

    feat: update AbortHandler and RemoveHandler for improved error handling and logging

--- cmd/AdminApi/AbortHandler.go
+++ cmd/AdminApi/AbortHandler.go
@@ -1,26 +1,31 @@
 package adminapi
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/fatih/color"
 	_ "github.com/moelksasbyahmed/go_loadbalancer/internal/server"
 )
 
 func (api *AdminAPi) AbortHandler(w http.ResponseWriter, r *http.Request) {
 
-	err := api.LBServer.CloseServer()
-	if err != nil {
-		http.Error(w, "Failed to abort the LoadBalancer server ", http.StatusInternalServerError)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	/*
-		er := api.server.Close()
-		if er != nil {
-			http.Error(w, "Failed to abort the Admin server ", http.StatusInternalServerError)
-			return
-		}
-	*/
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Server aborted successfully"))
+	log.Printf(color.RedString("Closing THE LOAD BALANCER"))
+
+	log.Printf(color.RedString("Closing THE ADMIN API"))
+
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		api.LBServer.HttpServer.Shutdown(ctx)
+		api.server.Shutdown(ctx)
+
+	}()
 
 }
--- cmd/AdminApi/RemoveHandler.go
+++ cmd/AdminApi/RemoveHandler.go
@@ -1,7 +1,36 @@
 package adminapi
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+
+	"net/http"
+	"net/url"
+
+	"github.com/moelksasbyahmed/go_loadbalancer/internal/server"
+)
 
 func (api *AdminAPi) handleRemoveServer(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+		return
+	}
+	var back backend
+	err := json.NewDecoder(r.Body).Decode(&back)
+	if err != nil {
+		http.Error(w, "Failed to decode request body", http.StatusBadRequest)
+	}
+	RMV_URL, _ := url.Parse(back.Url)
+	fmt.Println(back.Name)
+	err = api.LBServer.LB.RemoveBackend(&server.Backend{
+		Name: back.Name,
+		Url:  RMV_URL,
+	})
+	if err != nil {
+		http.Error(w, "Server Doesnt Exist", http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Server removed successfully"))
 }
